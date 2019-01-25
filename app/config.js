dfee3636dbaf46c2c8d497d5ba6729dcea923194
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2019-01-25 15:30:35 -0800

    basic node setup

---
+++ app/config.js
@@ -0,0 +1,22 @@
+/**
+ * For configuration, we will rely on process.env, a NodeJS global. To learn more:
+ * https://codeburst.io/process-env-what-it-is-and-why-when-how-to-use-it-effectively-505d0b2831e7 
+ */
+
+module.exports = {
+    // Check if the PORT environment variable exists. If not, default to 8080
+    PORT: process.env.PORT || 8080,
+    HTTP_STATUS_CODES: {
+        OK: 200,
+        CREATED: 201,
+        NO_CONTENT: 204,
+        BAD_REQUEST: 400,
+        UNAUTHORIZED: 401,
+        NOT_FOUND: 404,
+        INTERNAL_SERVER_ERROR: 500,
+    },
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/noted', //needs updating
+    TEST_MONGO_URL: process.env.TEST_MONGO_URL || 'mongodb://localhost:27017/test-noted', //needs updating
+    JWT_SECRET: process.env.JWT_SECRET || 'default',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
+};
---
+++ app/server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const { PORT, HTTP_STATUS_CODES, MONGO_URL, TEST_MONGO_URL } = require('./config');
+
+let server;
+//Init express server
+const app = express();
+
+//Set middleware
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.static('./public')); //Intercepts all HTTP requests that match files inside /public
+app.use('*', function (req, res) {
+    res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ error: 'Not found' });
+});
+
+module.exports = {
+    app,
+    startServer,
+    stopServer
+};
+
+function startServer(testEnv) {
+    return new Promise((resolve, reject) => {
+        let mongoUrl;
+
+        if (testEnv) {
+            mongoUrl = TEST_MONGO_URL;
+        } else {
+            mongoUrl = MONGO_URL;
+        }
+        // Step 1: Attempt to connect to MongoDB with mongoose
+        mongoose.connect(mongoUrl, { useNewUrlParser: true }, err => {
+            if (err) {
+                // Step 2A: If there is an error starting mongo, log error, reject promise and stop code execution.
+                console.error(err);
+                return reject(err);
+            } else {
+                // Step 2B: Start Express server
+                server = app.listen(PORT, () => {
+                    // Step 3A: Log success message to console and resolve promise.
+                    console.log(`Express server listening on http://localhost:${PORT}`);
+                    resolve();
+                }).on('error', err => {
+                    // Step 3B: If there was a problem starting the Express server, disconnect from MongoDB immediately, log error to console and reject promise.
+                    mongoose.disconnect();
+                    console.error(err);
+                    reject(err);
+                });
+            }
+        });
+    });
+}
+
+function stopServer() {
+    // Remember, because the process of starting/stopping a server takes time, it's preferrable to make
+    // this asynchronous, and return a promise that'll reject/resolve depending if the process is succesful.
+
+    // Step 1: Disconnect from the MongoDB database using Mongoose
+    return mongoose
+        .disconnect()
+        .then(() => new Promise((resolve, reject) => {
+            // Step 2: Shut down the ExpressJS server
+            server.close(err => {
+                if (err) {
+                    // Step 3A: If an error ocurred while shutting down, print out the error to the console and resolve promise;
+                    console.error(err);
+                    return reject(err);
+                } else {
+                    // Step 3B: If the server shutdown correctly, log a success message.
+                    console.log('Express server stopped.');
+                    resolve();
+                }
+            });
+        }));
+}
