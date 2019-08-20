3839ae4750f46510e746fc82c4b05122743d036d
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2019-08-20 08:24:01 -0500

    change db url

--- app/config.js
+++ app/config.js
@@ -1,22 +1,25 @@
 /**
  * For configuration, we will rely on process.env, a NodeJS global. To learn more:
- * https://codeburst.io/process-env-what-it-is-and-why-when-how-to-use-it-effectively-505d0b2831e7 
+ * https://codeburst.io/process-env-what-it-is-and-why-when-how-to-use-it-effectively-505d0b2831e7
  */
 
 module.exports = {
-    // Check if the PORT environment variable exists. If not, default to 8080
-    PORT: process.env.PORT || 8080,
-    HTTP_STATUS_CODES: {
-        OK: 200,
-        CREATED: 201,
-        NO_CONTENT: 204,
-        BAD_REQUEST: 400,
-        UNAUTHORIZED: 401,
-        NOT_FOUND: 404,
-        INTERNAL_SERVER_ERROR: 500,
-    },
-    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/noted', //needs updating
-    TEST_MONGO_URL: process.env.TEST_MONGO_URL || 'mongodb://localhost:27017/test-noted', //needs updating
-    JWT_SECRET: process.env.JWT_SECRET || 'default',
-    JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
+  // Check if the PORT environment variable exists. If not, default to 8080
+  PORT: process.env.PORT || 8080,
+  HTTP_STATUS_CODES: {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500
+  },
+  MONGO_URL:
+    process.env.MONGO_URL ||
+    "mongodb+srv://noted:noteduser@cluster0-pbybp.mongodb.net/noted?retryWrites=true&w=majority", //'mongodb://localhost:27017/noted', //needs updating
+  TEST_MONGO_URL:
+    process.env.TEST_MONGO_URL || "mongodb://localhost:27017/test-noted", //needs updating
+  JWT_SECRET: process.env.JWT_SECRET || "default",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "7d"
 };
