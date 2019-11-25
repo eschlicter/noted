3b1f60008a1cf90f538613d09c714cebd95dda22
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2019-11-25 16:29:39 -0600

    updating ui to bootstrap components

--- public/utilities/render.module.js
+++ public/utilities/render.module.js
@@ -5,7 +5,7 @@ window.RENDER_MODULE = {
 };
 
 function renderNotesList(notes) {
-  const notesHtml = notes.map(noteToHtml).join("<hr/>");
+  const notesHtml = notes.map(noteToHtml).join();
   $("#note-list").html(notesHtml);
 
   function noteToHtml(note) {
@@ -14,10 +14,12 @@ function renderNotesList(notes) {
       noteSummary = `${note.content.substring(0, 120)}...`;
     }
     return `
-        <div class="card text-center" id="note-card" data-note-id="${note.id}">
+        <div class="card border-info mb-3 content-align-center text-center" style="max-width: 30rem;" id="note-card" data-note-id="${
+          note.id
+        }">
             <h3 class="card-header">${note.title}<br>
-            <button id="edit-note-btn" class="btn btn-warning">Edit</button>
-            <button id="delete-note-btn" class="btn btn-danger">Delete</button></h3>
+            <button id="edit-note-btn" class="btn btn-outline-warning">Edit</button>
+            <button id="-note-btn" class="btn btn-outline-danger">Delete</button></h3>
             <p class="card-body card-content">${noteSummary}</p>
             <div class="card-footer text-muted card-info">
                 <i>${note.user.name} | Last updated on ${new Date(
@@ -35,7 +37,7 @@ function renderNoteDetails(note) {
   <div class="card-body">
     <h5 class="card-title">${note.title}</h5>
     <p class="card-text">${note.content}</p>
-    <button class="btn btn-primary" id="edit-note-btn">Edit Note</button>
+    <button class="btn btn-outline-primary" id="edit-note-btn">Edit Note</button>
   </div>
   <div class="card-footer text-muted">
   <i>${note.user.name} | ${new Date(note.updateDate).toLocaleString()}</i>
